84e210b03066fb0a4bcd47ce332b7e26ef204018
matthew-gigl-db <matthew-gigl-db>
2025-08-25 14:13:08 +0000

    set up the autoload and bronze and import configs

--- price_transparency/resources/ingestion_pipeline/data_sources/autoload.py
+++ price_transparency/resources/ingestion_pipeline/data_sources/autoload.py
@@ -1,15 +1,28 @@
+# The 'data_sources' folder contains definitions for all data sources
+# Keeping 'data_sources' separate provides a clear overview of the data used
+# and allows for easy swapping of sources during development.
+
 from utilities.bronze import Bronze
+import os
+import json
+config_dir = "../config/file_ingestion"
+json_files = [f for f in os.listdir(config_dir) if f.endswith('.json')]
 
-resource_types = spark.conf.get("resource_types").split(',')
-resource_types = [resource_type.strip() for resource_type in resource_types]
+definitions = []
+for json_file in json_files:
+    with open(os.path.join(config_dir, json_file), 'r') as file:
+        definitions.append(json.load(file))
 
-for resource_type in resource_types:
+for definition in definitions:
     BronzePipeline = Bronze(
         spark = spark
-        ,catalog = spark.conf.get("catalog_use")
-        ,schema = spark.conf.get("schema_use")
-        ,volume = spark.conf.get("volume_use")
-        ,volume_sub_path = spark.conf.get("volume_sub_path_use")
-        ,resource_type = resource_type)
+        ,catalog = spark.conf.get("catalog")
+        ,schema = spark.conf.get("schema")
+        ,volume = spark.conf.get("volume")
+        ,volume_sub_path = definition("volume_sub_path")
+        ,file_type = definition["file_type"]
+        ,cleanSource = definition["cleanSource"]
+        ,cleanSource_retentionDuration = definition["cleanSource_retentionDuration"]
+    )
     
     BronzePipeline.stream_ingest()
--- price_transparency/resources/ingestion_pipeline/utilities/bronze.py
+++ price_transparency/resources/ingestion_pipeline/utilities/bronze.py
@@ -1,16 +1,21 @@
 import dlt
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, row_number
+from pyspark.sql.functions import col, row_number, lit, concat
 from pyspark.sql.window import Window
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType, MapType
 
 class Bronze:
-    def __init__(self, spark: SparkSession, catalog: str, schema: str, volume: str, volume_sub_path: str, resource_type: str):
+    def __init__(self, spark: SparkSession, catalog: str, schema: str, volume: str, volume_sub_path: str, file_type: str, file_desc: str, cleanSource_retentionDuration: str, cleanSource: str = "OFF"):
         self.spark = spark
         self.catalog = catalog
         self.schema = schema
         self.volume = volume
         self.volume_sub_path = volume_sub_path
-        self.resource_type = resource_type
+        self.file_type = file_type
+        self.file_desc = file_desc
+        self.cleanSource_retentionDuration = cleanSource_retentionDuration
+        self.cleanSource = cleanSource
     """
     The Bronze class represents a data structure for managing metadata related to a specific data resource.
     
@@ -18,17 +23,36 @@ class Bronze:
         spark (SparkSession): The SparkSession object used for interacting with the Spark runtime.
         catalog (str): The catalog name where the data is stored.
         schema (str): The schema name within the catalog.
+        volume (str): The volume name where the data is stored.
         volume_sub_path (str): The sub-path within the volume where the data is located.
+        file_type (str): The type of the data resource.
+        file_desc (str): Description of the file type.
+        cleanSource_retentionDuration (str): Retention duration for cleaning the source.
+        cleanSource (str): Clean source option, default is "OFF".
 
     Methods:
         __repr__(): Returns a string representation of the Bronze object.
-        stream_ingest(): Defines a Delta Live Table for streaming ingestion of CSV files.
+        stream_ingest(): Defines a Delta Live Table for streaming ingestion of files.
         to_dict(): Converts the Bronze object attributes to a dictionary.
         from_dict(cls, data): Creates a Bronze object from a dictionary.
     """
 
     def __repr__(self):
-        return f"Bronze(catalog='{self.catalog}', schema='{self.schema}', volume='{self.volume}',volume_sub_path='{self.volume_sub_path}'"
+        return (f"Bronze(spark={self.spark!r}, catalog={self.catalog!r}, schema={self.schema!r}, "
+                f"volume={self.volume!r}, volume_sub_path={self.volume_sub_path!r}, "
+                f"file_type={self.file_type!r}, file_desc={self.file_desc!r}, "
+                f"cleanSource_retentionDuration={self.cleanSource_retentionDuration!r}, "
+                f"cleanSource={self.cleanSource!r})")
+
+    @staticmethod
+    @udf(MapType(StringType(), StringType()))
+    def copy_file(src_path, dest_path):
+      try:
+        with open(src_path, 'rb') as src_file, open(dest_path, 'wb') as dest_file:
+          dest_file.write(src_file.read())
+        return {"status": "success"}
+      except Exception as e:
+        return {"status": "error", "message": str(e)}
       
     def stream_ingest(self):
       schema_definition = f"""
@@ -39,17 +63,16 @@ class Bronze:
         file_block_length: BIGINT,
         file_modification_time: TIMESTAMP > NOT NULL COMMENT 'Metadata about the file ingested.'
         ,ingest_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date timestamp the file was ingested.'
-        ,value STRING COMMENT 'The raw CSV file contents.'
+        ,value STRING COMMENT 'The raw {self.file_desc} file contents.'
       """
 
-      if self.volume_sub_path == None:
-        volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}"
-      else:
-        volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}/{self.volume_sub_path}"
+      volume_path = f"/Volumes/{self.catalog}/{self.schema}/{self.volume}"
+      if self.volume_sub_path:
+        volume_path = f"{volume_path}/{self.volume_sub_path}"
 
       @dlt.table(
-        name=f"{self.catalog}.{self.schema}.{self.resource_type}_bronze",
-        comment=f"Streaming bronze ingestion of price transparency files.",
+        name=f"{self.catalog}.{self.schema}.{self.file_type}_bronze",
+        comment=f"Streaming bronze ingestion of {self.file_type} files from {volume_path}",
         # spark_conf={"<key>" : "<value>", "<key>" : "<value>"},
         table_properties={
           'quality' : 'bronze'
@@ -59,6 +82,7 @@ class Bronze:
         },
         # path="<storage-location-path>",
         # partition_cols=["<partition-column>", "<partition-column>"],
+        cluster_by_auto=True,
         cluster_by = ["file_metadata.file_path"],
         schema=schema_definition,
         # row_filter = "row-filter-clause",
@@ -69,16 +93,40 @@ class Bronze:
           return (self.spark.readStream
             .format("cloudFiles")
             .option("cloudFiles.format", "text")
+            .option("wholeText", "true")
+            .option("cloudFiles.cleanSource", self.cleanSource)
+            .option("cloudFiles.cleanSource.retentionDuration", self.cleanSource_retentionDuration)
             .load(volume_path)
             .selectExpr("_metadata as file_metadata", "*")
           )
 
     def to_dict(self):
-        return {"spark": self.spark, "catalog": self.catalog, "schema": self.schema, "volume_sub_path": self.volume_sub_path, "resource_type": self.resource_type}
+        return {
+            "spark": self.spark,
+            "catalog": self.catalog,
+            "schema": self.schema,
+            "volume": self.volume,
+            "volume_sub_path": self.volume_sub_path,
+            "file_type": self.file_type,
+            "file_desc": self.file_desc,
+            "cleanSource_retentionDuration": self.cleanSource_retentionDuration,
+            "cleanSource": self.cleanSource
+        }
 
     @classmethod
     def from_dict(cls, data):
-        return cls(data['spark'], data['catalog'], data['schema'], data['volume_sub_path'], data['resource_type'])
+        return cls(
+            spark=data['spark'],
+            catalog=data['catalog'],
+            schema=data['schema'],
+            volume=data['volume'],
+            volume_sub_path=data['volume_sub_path'],
+            file_type=data['file_type'],
+            file_desc=data['file_desc'],
+            cleanSource_retentionDuration=data['cleanSource_retentionDuration'],
+            cleanSource=data.get('cleanSource', "OFF")
+        )
+
 
 
 
