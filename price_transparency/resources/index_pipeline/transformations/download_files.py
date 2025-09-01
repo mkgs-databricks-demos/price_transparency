381340134c9b9d7d6c1731d76f05ae418b97b319
matthew-gigl-db <matthew-gigl-db>
2025-09-01 19:44:08 +0000

    update and rename the pipeline for the index

---
+++ price_transparency/resources/index_pipeline/transformations/download_files.py
@@ -0,0 +1,10 @@
+import dlt
+from utilities import *
+
+@dlt.table(
+)
+def download_files():
+  df = (
+    spark.read.table("allowed_amount_file_index")
+    .filter()
+  )
--- price_transparency/resources/ingestion_pipeline/transformations/download_files.py
+++
@@ -1,7 +0,0 @@
-import dlt
-from utilities import *
-
-@dlt.table(
-)
-def download_files():
-  df = spark.read.table("allowed_amount_file_index")
