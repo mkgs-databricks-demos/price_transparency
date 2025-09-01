6477ebc3b71cadd4ec3f0cd08e03d58dcaaaea31
matthew-gigl-db <matthew-gigl-db>
2025-09-01 20:04:45 +0000

    add allowed amount cdf view and start of mv

--- price_transparency/resources/index_pipeline/transformations/download_files.py
+++ price_transparency/resources/index_pipeline/transformations/download_files.py
@@ -1,10 +1,16 @@
 import dlt
 from utilities import *
+from pyspark.sql.functions import col
+
+@dlt.view()
+def allowed_amount_file_index_cdf():
+  return spark.readStream.option("readChangeFeed", "true").table("allowed_amount_file_index")
 
 @dlt.table(
 )
 def download_files():
   df = (
-    spark.read.table("allowed_amount_file_index")
-    .filter()
+    spark.read.table("allowed_amount_file_index_cdf")
+    .filter(col("file_downloaded").isNull())
+    .filter(col("file_location").isNotNull())
   )
