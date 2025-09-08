705378ef1115614a69ee445c611702808dddc641
matthew-gigl-db <matthew-gigl-db>
2025-09-08 01:48:48 +0000

    use test files for in-network, switch to config style

--- price_transparency/resources/machine_readable_files/transformations/bronze.py
+++ price_transparency/resources/machine_readable_files/transformations/bronze.py
@@ -1,28 +1,30 @@
 from utilities.utils import MachineReadableFiles
+import os
+import json
+config_dir = "../config"
+definition_files = [f for f in os.listdir(config_dir) if f.endswith('def.json')]
 
-definition = {
-  "file_type": "in_network_json",
-  "file_desc": "in network price transparency json",
-  "file_extention": "json",
-  "cleanSource": "OFF",
-  "cleanSource_retentionDuration": "7 days",
-  "volume_sub_path": "in-network/uncompressed",
-  "maxFilesPerTrigger": 100,
-  "cloudFiles_useNotifications": "false"
-}
+definitions = []
+for definition_file in definition_files:
+    with open(os.path.join(config_dir, definition_file), 'r') as file:
+        definitions.append(json.load(file))
 
-mrf = MachineReadableFiles(
-    spark = spark
-    ,catalog = "mgiglia"
-    ,schema = "dev_matthew_giglia_price_transparency"
-    ,volume= "landing"
-    ,volume_sub_path=definition.get("volume_sub_path", "in-network/uncompressed")
-    ,file_type=definition.get("file_type", "in_network_json")
-    ,file_desc=definition.get("file_desc")
-    ,cleanSource=definition.get("cleanSource", "OFF")
-    ,cleanSource_retentionDuration=definition.get("cleanSource_retentionDuration", "7 days")
-    ,maxFilesPerTrigger=definition.get("maxFilesPerTrigger", 100)
-    ,cloudFiles_useNotifications=definition.get("cloudFiles_useNotifications", "true")
-)
+for definition in definitions:
+  mrf = MachineReadableFiles(
+      spark = spark
+      ,catalog = "mgiglia"
+      ,schema = "dev_matthew_giglia_price_transparency"
+      ,volume= "landing"
+      ,volume_sub_path=definition.get("volume_sub_path")
+      ,file_type=definition.get("file_type")
+      ,file_desc=definition.get("file_desc")
+      ,useSchemaHints = definition.get("useSchemaHints")
+      ,schemaHints = definition.get("schemaHints")
+      ,jsonSchema = definition.get("jsonSchema")
+      ,cleanSource=definition.get("cleanSource", "OFF")
+      ,cleanSource_retentionDuration=definition.get("cleanSource_retentionDuration", "7 days")
+      ,maxFilesPerTrigger=definition.get("maxFilesPerTrigger", 100)
+      ,cloudFiles_useNotifications=definition.get("cloudFiles_useNotifications", "true")
+  )
 
-mrf.ingest()
+  mrf.ingest()
